d717d1098d44018646ad747024c8ae3d82fb5506
nontan-rh <niszkml@gmail.com>
2017-06-17 19:53:01 +0900

    荒ぶりのスイーツ

--- app/src/main/java/me/nontan/spajam_sweets_kun/MainMapsActivity.kt
+++ app/src/main/java/me/nontan/spajam_sweets_kun/MainMapsActivity.kt
@@ -1,20 +1,26 @@
 package me.nontan.spajam_sweets_kun
 
 import android.os.Bundle
+import android.os.Handler
 import android.support.v4.app.FragmentActivity
-import android.view.View
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Marker
 import com.google.android.gms.maps.model.MarkerOptions
+import me.nontan.spajam_sweets_kun.models.SweetsKun
+import java.util.*
+import kotlin.concurrent.timer
 
 class MainMapsActivity : FragmentActivity(), OnMapReadyCallback {
-
+    private val handler: Handler = Handler()
     private var mMap: GoogleMap? = null
-    private var sweetsViews: Array<View> = arrayOf()
+    private var sweetsKuns: Array<SweetsKun> = arrayOf()
+    private var sweetsKunMarker: HashMap<SweetsKun, Marker> = hashMapOf()
+    private var sweetsKunPosition: HashMap<SweetsKun, LatLng> = hashMapOf()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -39,14 +45,42 @@ class MainMapsActivity : FragmentActivity(), OnMapReadyCallback {
 
         // Add a marker in Sydney and move the camera
         val sydney = LatLng(-34.0, 151.0)
-        mMap!!.isIndoorEnabled = false
-        mMap!!.isTrafficEnabled = false
-        mMap!!.isBuildingsEnabled = false
-        val marker = MarkerOptions().position(sydney).icon(BitmapDescriptorFactory.fromResource(R.drawable.cake_kun))
-        mMap!!.addMarker(marker)
-        mMap!!.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        googleMap.isIndoorEnabled = false
+        googleMap.isTrafficEnabled = false
+        googleMap.isBuildingsEnabled = false
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+
+        sweetsKuns = arrayOf(SweetsKun(0, 0, 35.0, 135.0))
+        onSweetsViewsUpdated()
     }
 
     fun onSweetsViewsUpdated() {
+        val googleMap = mMap?.let { it } ?: return
+        for (sweetsKun in sweetsKuns) {
+            val pos = LatLng(sweetsKun.latitude, sweetsKun.longitude)
+            val markerOpt = MarkerOptions().position(pos).icon(BitmapDescriptorFactory.fromResource(R.drawable.cake_kun))
+            val marker = googleMap.addMarker(markerOpt)
+
+            sweetsKunMarker[sweetsKun] = marker
+            sweetsKunPosition[sweetsKun] = pos
+        }
+
+        timer("randomWalkTimer", false, 0, 500, {
+            randomWalkSweetsKun()
+        })
+    }
+
+    fun randomWalkSweetsKun() {
+        handler.post {
+            for (sweetsKun in sweetsKuns) {
+                val marker = sweetsKunMarker[sweetsKun].let { it } ?: continue
+                val currentPos = sweetsKunPosition[sweetsKun].let { it } ?: continue
+                val newLatitude = currentPos.latitude + Math.random() * 0.001
+                val newLongitude = currentPos.longitude + Math.random() * 0.001
+                val newPos = LatLng(newLatitude, newLongitude)
+
+                marker.position = newPos
+            }
+        }
     }
 }
--- app/src/main/java/me/nontan/spajam_sweets_kun/models/SweetsKun.kt
+++ app/src/main/java/me/nontan/spajam_sweets_kun/models/SweetsKun.kt
@@ -1,9 +1,8 @@
 package me.nontan.spajam_sweets_kun.models
 
-enum class
-
 data class SweetsKun(
     val id: Int,
-    val name: Int,
-    val hasFamily: Boolean
+    val type: Int,
+    val latitude: Double,
+    val longitude: Double
 )
