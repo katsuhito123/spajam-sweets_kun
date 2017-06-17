f69edec9c10f72b6a53c792c0c8f65a4e3df6064
nontan-rh <niszkml@gmail.com>
2017-06-18 04:29:50 +0900

    レビュー送信！

--- app/src/main/java/me/nontan/spajam_sweets_kun/ReviewActivity.kt
+++ app/src/main/java/me/nontan/spajam_sweets_kun/ReviewActivity.kt
@@ -2,17 +2,23 @@ package me.nontan.spajam_sweets_kun
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
 import android.widget.*
-import com.google.gson.Gson
+import me.nontan.spajam_sweets_kun.models.ReviewCreateRequest
+import me.nontan.spajam_sweets_kun.models.ReviewCreateResponse
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class ReviewActivity : AppCompatActivity() {
-    var img: ImageView? = null
-    var rateBar:RatingBar? = null
-    var swtSpn:Spinner? = null
-    var review:EditText? = null
-    var sendBtn:Button? = null
-    var id:Int? = null
-    var rate:Int? = null
+    lateinit var img: ImageView
+    lateinit var rateBar: RatingBar
+    lateinit var swtSpn: Spinner
+    lateinit var review: EditText
+    lateinit var sendBtn: Button
+
+    var id: Int = 0
+    var rate: Int = 3
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,26 +31,30 @@ class ReviewActivity : AppCompatActivity() {
         sendBtn = findViewById(R.id.button) as Button
 
         val intent = this.intent
-        id = intent.getIntExtra("shop_id",0)
+        id = intent.getIntExtra("shop_id", 0)
         rate = 3;
 
-        rateBar!!.setOnRatingBarChangeListener { ratingBar, rating, fromUser ->
+        rateBar.setOnRatingBarChangeListener { ratingBar, rating, fromUser ->
             rate = rating.toInt()
         }
 
-        sendBtn!!.setOnClickListener {
-            val shop_id = id!!
-            val rateNum = rate!!
-            val review_text = review!!.text.toString()
-            val sweet_type = swtSpn!!.selectedItemPosition
-//        intent = Intent(this,MainMapsActivity::class.java)
-//        intent.putExtra("shop_id",shop_id)
-//            Toast.makeText(this,"id = "+shop_id+",rateNum = "+rateNum+ ",review_text = "+review_text+",sweet_type = "+sweet_type,Toast.LENGTH_SHORT).show()
+        sendBtn.setOnClickListener {
+            val shop_id = id
+            val rateNum = rate
+            val review_text = review.text.toString()
+            val sweet_type = swtSpn.selectedItemPosition
 
-            //var gson: Gson = Gson()
-            //val review:Review = Review(shop_id,rateNum,review_text,sweet_type)
-//            gson.toJson(review)
-            //Toast.makeText(this,gson.toJson(review).toString(),Toast.LENGTH_LONG).show()
+            val request = ReviewCreateRequest(shop_id, rateNum, review_text, sweet_type)
+            val call = sharedAPIInstance.reviewCreate("Bearer " + accessToken, request)
+            call.enqueue(object: Callback<ReviewCreateResponse> {
+                override fun onResponse(call: Call<ReviewCreateResponse>, response: Response<ReviewCreateResponse>) {
+                    Log.d("reviewcreate", "Response: " + response.body().toString());
+                    Log.d("reviewcreate", "Error: " + response.errorBody()?.string().toString())
+                }
+                override fun onFailure(call: Call<ReviewCreateResponse>?, t: Throwable?) {
+                    Log.d("reviewcreate", t.toString())
+                }
+            })
         }
     }
 }
